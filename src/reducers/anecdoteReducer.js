a8ea1d52b2f586187a7487deb3d027372563219c
Andrew Moore <andrewdmoore84@gmail.com>
2022-05-06 23:20:38 -0400

    Add dev DB API and use it to load starting data on page load

--- src/reducers/anecdoteReducer.js
+++ src/reducers/anecdoteReducer.js
@@ -1,12 +1,3 @@
-const anecdotesAtStart = [
-  'If it hurts, do it more often',
-  'Adding manpower to a late software project makes it later!',
-  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-  'Premature optimization is the root of all evil.',
-  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
-]
-
 const getId = () => (100000 * Math.random()).toFixed(0)
 
 const asObject = (anecdote) => {
@@ -37,7 +28,16 @@ export const createAnecdote = (anecdote) => {
   return newAnecdote
 }
 
-const initialState = anecdotesAtStart.map(asObject)
+export const setAnecdotes = (anecdotes) => {
+  const allAnecdotes = {
+    type: 'SET_ANECDOTES',
+    payload: anecdotes
+  }
+
+  return allAnecdotes
+}
+
+const initialState = []
 
 const anecdoteReducer = (state = initialState, action) => {
   // console.log('state now: ', state)
@@ -46,6 +46,9 @@ const anecdoteReducer = (state = initialState, action) => {
   switch (action.type) {
     case 'NEW_ANECDOTE': {
       return sortByVotes([...state, action.payload])
+    }
+    case 'SET_ANECDOTES': {
+      return action.payload
     }
     case 'VOTE': {
       const id = action.payload.id

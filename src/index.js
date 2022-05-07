862eccceb4a1c7189687241ca37e02d72e7bc5e2
Andrew Moore <andrewdmoore84@gmail.com>
2022-05-07 15:56:17 -0400

    Remove old code from index.js

--- src/index.js
+++ src/index.js
@@ -4,11 +4,6 @@ import { Provider } from 'react-redux'
 import App from './App'
 
 import store from './store'
-import { setAnecdotes } from './reducers/anecdoteReducer'
-import anecdoteService from './services/anecdotes'
-
-anecdoteService.getAll()
-  .then(anecdotes => store.dispatch(setAnecdotes(anecdotes)))
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
